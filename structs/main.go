876fe066dee784b41cb8c6fa0a4084f885818af1
julius.suominen <julius.suominen@gmail.com>
2021-02-24 21:28:12 +0200

    add structs

---
+++ structs/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+	age       int
+	height    float32
+	isTrue    bool
+}
+
+func main() {
+	// alex := person{firstName: "Alex", lastName: "Anonymous"}
+	var alex person // zero value struct assigned to alex
+	fmt.Println(alex)
+	// %+v prints out all field names and values from alex
+	fmt.Printf("%+v", alex)
+}
