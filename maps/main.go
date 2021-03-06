71814c9b5f333345f7baa82f5270590f78bc0168
julius.suominen <julius.suominen@gmail.com>
2021-03-06 13:16:04 +0200

    add printMap

--- maps/main.go
+++ maps/main.go
@@ -3,22 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	// var colors map[string]string
-	colors := make(map[string]string)
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00FF00",
+		"white": "#ffffff",
+	}
 
 	/*
-		colors = map[string]string{
-			"red":   "#ff0000",
-			"green": "#00FF00",
-		}
+		colors["white"] = "#ffffff"
+		// map keys have to accessed with square brackets because map keys are typed
+		fmt.Println(colors["white"])
+
+		delete(colors, "white")
 	*/
+	printMap(colors)
+}
 
-	colors["white"] = "#ffffff"
-
-	// map keys have to accessed with square brackets because map keys are typed
-	fmt.Println(colors["white"])
-
-	delete(colors, "white")
-
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is ", hex)
+	}
 }
