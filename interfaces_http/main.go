a967ed8fe9e8f7d656d68f8bddf4fb42d2d1e4f1
julius.suominen <julius.suominen@gmail.com>
2021-03-06 14:27:32 +0200

    implement a Writer

--- interfaces_http/main.go
+++ interfaces_http/main.go
@@ -7,12 +7,23 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err != nil {
 		fmt.Println("error: ", err)
 		os.Exit(1)
 	}
+
+	ln := logWriter{}
 	// destination is a Writer, src is a Reader
-	io.Copy(os.Stdout, resp.Body)
+	io.Copy(ln, resp.Body)
+}
+
+// logWriter implements the Writer interface now
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes: ", len(bs))
+	return len(bs), nil
 }
