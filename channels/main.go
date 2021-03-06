00f63da8a70938491868614cc475e78a6276d447
julius.suominen <julius.suominen@gmail.com>
2021-03-06 18:04:15 +0200

    add sleep, pass value to routine to fix bug

--- channels/main.go
+++ channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -21,28 +22,25 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	// a value coming through channel c is a blocking call
-	// this loop will block once a message comes and prints it out
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	// wait for the channel to return a value and then assign it to l
+	// i.e. call checkLink for each link forever
+	for l := range c {
+		// use function literal to pause only when this infinite loop does an iteration,
+		// not during the initial loop above
+		go func(link string) {
+			time.Sleep(3 * time.Second)
+			checkLink(link, c)
+		}(l)
 	}
-
-	/*
-		could also be:
-
-		for range links {
-			fmt.Println(<-c)
-		}
-	*/
 }
 
 func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 	fmt.Println(link, "is up.")
-	c <- "Yep it's up."
+	c <- link
 }
